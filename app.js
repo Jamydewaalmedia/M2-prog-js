30165e413c2f93b730929b6cdd6243a01a3f6784
jamydewaal <35739@ma-web.nl>
2022-11-21 10:09:47 +0100

    opdracht af

---
+++ app.js
@@ -0,0 +1,31 @@
+class App
+{
+    runApplication()
+    {
+        //code gaat hier!
+
+        let appName = "name";
+
+        let versienummer = 0.9;
+
+        let versiedatum = "21-11-2022";
+
+        let autheur = "jamy";
+
+        let copyright = "Copyright jamy 2022";
+
+        let distributeur = "ja patatje";
+
+        let darkmode = true;
+        console.log(appName);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+}
+
+let app = new App();
+app.runApplication();
